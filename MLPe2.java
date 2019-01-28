8851331b61a9f8428381f6bf1b985c546ba02510
Morgan Parker <missmoparker@gmail.com>
2019-01-28 17:33:41 -0600

    Add files via upload

---
+++ MLPe2.java
@@ -0,0 +1,41 @@
+//I liked playing around with the print and println differences
+//I am still trying to figure out how to use the String command in different ways without producing an error
+//Can you only use String with the length command or as a variable?
+//Can this be a real check?
+//1011 is my lucky number - there are about 10^11 stars in the galaxy and 10^11 bactera on your body
+//funny coincidence right?
+/* Morgan Parker
+CSCE 111
+Exercise 2
+1/21/19
+*/
+public class MLPe2
+{
+   public static void main( String[] args )
+   {
+      System.out.println( "+___________________________________________________+");
+      System.out.println( "|                                        1011       |" );
+      System.out.println( "| Texas A&M University                              |" );
+      System.out.println( "|                          DATE__Jan._21,_2019_     |" );
+      System.out.println( "|  PAY TO THE                                       |" );
+      System.out.println( "|  ORDER OF_____Morgan_Parker________   $100,000.00 |" );
+      System.out.println( "|                                                   |" );
+      System.out.println( "|  MEMO___Scholarship_Money_  __President_Young___  |" );
+      System.out.println( "|                                                   |" );
+      System.out.println( "|    :123456789:  :123456789:     1011              |" );
+      System.out.println( "+___________________________________________________+");
+
+      String name = "Morgan";
+      String goal = "money";
+      int alot = 100000;
+      System.out.println();
+      System.out.println("I want "+goal);
+      System.out.println("We all want " +goal);
+      System.out.println();
+      System.out.println("I want to go to grad school and get my PhD.");
+      System.out.println("I am so glad that I got this check.");
+      System.out.println("Don't make me pay for college");
+      System.out.println("How much money? " +alot);
+      System.out.println();
+                      }// end main
+                            }// end class
